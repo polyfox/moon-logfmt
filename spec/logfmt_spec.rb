5ff8b3a54340c74330b9e33a26f5a6b3007bb14a
Corey Powell <mistdragon100@gmail.com>
2015-05-01 15:51:58 -0500

    Updated specs

--- spec/logfmt_spec.rb
+++ spec/logfmt_spec.rb
@@ -3,5 +3,9 @@ require 'moon-logfmt/logfmt'
 
 describe Moon::Logfmt do
   context '.format_context' do
+    it 'formats a Hash to a String' do
+      actual = described_class.format_context(msg: 'Hello World', nums: [1, 2, 3])
+      expect(actual).to eq('msg="Hello World" nums=1,2,3')
+    end
   end
 end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -3,14 +3,31 @@ require 'moon-logfmt/logfmt'
 
 describe Moon::Logfmt::Logger do
   context '#initialize' do
+    it 'initializes a new logger without arguments' do
+      logger = described_class.new
+    end
+
+    it 'initializes a new logger with context arguments' do
+      logger = described_class.new fn: 'test'
+
+      expect(logger.send(:context)).to eq(fn: 'test')
+    end
   end
 
   context '#initialize_copy' do
+    it 'initializes a copy' do
+      logger = described_class.new fn: 'test'
+      c = logger.dup
+
+      expect(c.send(:context)).to eq(logger.send(:context))
+      expect(c.send(:context)).not_to equal(logger.send(:context))
+    end
   end
 
   context '#write' do
-  end
-
-  context '#new' do
+    it 'writes a new log line' do
+      logger = Moon::Logfmt::NullLogger.new fn: 'test'
+      logger.write msg: 'Test Test Test'
+    end
   end
 end
